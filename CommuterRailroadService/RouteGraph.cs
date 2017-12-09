59e444e697084e7f34c5e237eb1bbf4b7c6a2f82
Atif Khokhar <atif_747@hotmail.co.uk>
2017-12-09 18:17:05 +0000

    Created RouteGraph which is a representation of a Directional Weighted Graph Also added Functionality to Create a Rail Link from one station to another.
    Created RouteGraph which is a representation of a Directional Weighted
    Graph Also added Functionality to Create a Rail Link  (Edge) from one
    station (Vertex) to another. Unit test to cover this created.

---
+++ CommuterRailroadService/RouteGraph.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace CommuterRailroadService
+{
+    public class RouteGraph
+    {
+        public List<Station> stations; 
+
+        public RouteGraph()
+        {
+            stations = new List<Station>();
+        }
+
+        public void CreateRailLinkForStation(Station origin,Station destination,int distance)
+        {
+            stations.Add(origin);
+            stations.Add(destination);
+            origin.railLinks.Add(new RailLink()
+            {
+                origin = origin,
+                destination = destination,
+                distance = distance
+            });
+        }
+    }
+}
--- CommuterRailroadService/RouteGraphShould.cs
+++ CommuterRailroadService/RouteGraphShould.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using NUnit.Framework;
+
 namespace CommuterRailroadService
 {
-    public class GraphShould
+    [TestFixture]
+    public class RouteGraphShould
     {
-        public GraphShould()
+        [Test]
+        public void CreateRailLinkFromOneStationToAnother()
         {
+            var station1 = new Station("A");
+            var station2 = new Station("B");
+            var sut = new RouteGraph();
+            var expectedRailLink = new RailLink()
+            {
+                origin = station1,
+                destination = station2,
+                distance = 5
+            };
+
+
+            sut.CreateRailLinkForStation(station1,station2,5);
+
+            Assert.That(station1.railLinks[0].origin.Equals(expectedRailLink.origin));
+            Assert.That(station1.railLinks[0].destination.Equals(expectedRailLink.destination));
+            Assert.That(station1.railLinks[0].distance.Equals(expectedRailLink.distance));
         }
     }
 }
