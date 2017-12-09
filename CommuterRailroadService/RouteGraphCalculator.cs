59fd498fb4a7c957cae0f9e68c5ed53de48ce60d
Atif Khokhar <atif_747@hotmail.co.uk>
2017-12-09 21:29:42 +0000

    Added in RouteGraphCalculator that runs calculations on a Route. Also created a method that calculates route for 2 stations.
    Added in RouteGraphCalculator that runs calculations on a Route. Also
    created a method that calculates route for 2 connected stations. With
    Unit Tests.

---
+++ CommuterRailroadService/RouteGraphCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace CommuterRailroadService
+{
+    public class RouteGraphCalculator
+    {
+
+        private RouteGraph routeGraph;
+
+        public RouteGraphCalculator(RouteGraph routeGraph)
+        {
+            this.routeGraph = routeGraph;
+        }
+
+        public int CalculateDistance(string stationNameA, string stationNameB)
+        {
+
+            Station routeOrigin = routeGraph.stations.Find(o => o.name == stationNameA);
+            var routeDestination = routeGraph.stations.Find(d => d.name == stationNameB);
+
+           var routeDistances = routeOrigin.railLinks.Where(rl => rl.destination == routeDestination)
+                                       .Select(rl => rl.distance).Distinct();
+                                      
+
+            return routeDistances.Sum();
+        }
+    }
+}
---
+++ CommuterRailroadService/RouteGraphCalculatorShould.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CommuterRailroadService
+{
+    [TestFixture]
+    public class RouteGraphCalculatorShould
+    {
+        private Station stationA;
+        private Station stationB;
+        private Station stationC;
+        private Station stationD;
+        private RouteGraph graph;
+        private RouteGraphCalculator sut;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            stationA = new Station("A");
+            stationB = new Station("B");
+            stationC = new Station("C");
+            stationD = new Station("D");
+
+            graph = new RouteGraph();
+
+            graph.CreateRailLinkForStation(stationA, stationB, 5);
+            graph.CreateRailLinkForStation(stationB, stationC, 4);
+
+        }
+        
+        [Test]
+        public void CalculateDistanceBetweenTwoLinkedStations()
+        {
+            sut = new RouteGraphCalculator(graph); 
+
+            var actualResult = this.sut.CalculateDistance("A", "B");
+
+            Assert.That(actualResult.Equals(5)); 
+
+        }
+
+
+    }
+
+}
